992dbd8e99519babf14376c797bcd7be1c288bb1
Logan G <logangerber1014@yahoo.com>
2020-01-12 03:31:06 -0800

    Create router for getting posts from mongodb

---
+++ routes/entry.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var assert = require("assert");
+var MongoClient = require("mongodb").MongoClient;
+var router = express.Router();
+
+var sites;
+
+const mongoClient = new MongoClient("mongodb://localhost:27017", {useUnifiedTopology: true});
+
+mongoClient.connect((err) => {
+    assert.equal(err, null);
+    sites = mongoClient.db('website-test').collection('pages');
+})
+
+router.get("/:articleTitle", function(req, res, next) {
+    sites.find({url: req.params["articleTitle"]}).toArray((function(err, docs) {
+        if(err){
+            console.log(err);
+            res.send("unu");
+            return;
+        }
+        let pageText = docs[0]["text"].split("\n\n");
+        let title = pageText[0];
+        pageText = pageText.slice(1);
+
+        res.render("blog_entry", {title: title, paragraphs: pageText, imageInfos: []});
+    }));
+});
+
+module.exports = router;
